d6094df70101869ec9345efbf39c70dba5d7deb6
nicolas contreras porto <nicolascontrerasporto@gmail.com>
2021-09-25 22:04:37 -0300

    cree barra de menu / contreras porto

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+const NavBar = () =>{
+return(
+    <Navbar bg="dark" variant="dark" expand="lg">
+    <Container>
+      <Navbar.Brand href="#home">Ecommerce</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home">camisetas</Nav.Link>
+          <Nav.Link href="#link">zapatillas</Nav.Link>
+          <Nav.Link href="#">pantalones</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+)
+}
+export default NavBar
